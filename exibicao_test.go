8b854e88224523c956fd28124cfa7cd88123d1b2
Enzo Santos <enzo.santos29@hotmail.com>
2024-04-06 09:10:18 -0300

    Adiciona caso de teste em que origem é exibida

--- exibicao_test.go
+++ exibicao_test.go
@@ -39,6 +39,27 @@ func TestExibicao_1Octante_Crescente(t *testing.T) {
 	require.Equal(t, " ¹⁰¹²³⁴⁵⁶", lines[6])
 }
 
+func TestExibicao_123Quadrantes_Crescente(t *testing.T) {
+	var writer strings.Builder
+	require.Nil(t, Exibe(
+		NewAlgoritmoBresenham(Ponto{X: -4, Y: -3}, Ponto{X: 2, Y: 5}),
+		&writer,
+	))
+	lines := strings.Split(writer.String(), "\n")
+	require.Equal(t, "⁶ ┊   ┃ ┊ ", lines[0])
+	require.Equal(t, "⁵╌┊╌╌╌┃╌█╌", lines[1])
+	require.Equal(t, "⁴ ┊   ┃░┊ ", lines[2])
+	require.Equal(t, "³ ┊   ░ ┊ ", lines[3])
+	require.Equal(t, "² ┊   ░ ┊ ", lines[4])
+	require.Equal(t, "¹ ┊  ░┃ ┊ ", lines[5])
+	require.Equal(t, "⁰━━━░━╋━━━", lines[6])
+	require.Equal(t, "¹ ┊░  ┃ ┊ ", lines[7])
+	require.Equal(t, "² ┊░  ┃ ┊ ", lines[8])
+	require.Equal(t, "³╌█╌╌╌┃╌┊╌", lines[9])
+	require.Equal(t, "⁴ ┊   ┃ ┊ ", lines[10])
+	require.Equal(t, " ⁵⁴³²¹⁰¹²³", lines[11])
+}
+
 func TestExibicao_2Octante_Crescente(t *testing.T) {
 	var writer strings.Builder
 	require.Nil(t, Exibe(
